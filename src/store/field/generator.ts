5be8cd04cecf7841ec9c8e106b5d70fc9225a5db
Dima <dimafilin6@icloud.com>
2022-11-26 18:26:18 +0700

    Заполнение минами

--- src/store/field/generator.ts
+++ src/store/field/generator.ts
@@ -1,5 +1,6 @@
 import { GameSettings } from '../game/types';
-import { Cell, Field } from './types';
+import { Cell, Field, MinesAround } from './types';
+import { generateRandomNumber } from '../../utils/random-number';
 
 function defaultCell(): Cell {
     return {
@@ -11,6 +12,9 @@ function defaultCell(): Cell {
 }
 
 function defaultField(settings: GameSettings): Field {
+    // height - количество массивов [ [ 1 2 3 ] [4 5 6] ] - height: 2, width: 3
+    // width - размер массива
+    // const positionsLeft = settings.size.width * settings.size.height;
     const fieldHeight: Cell[][] = new Array(settings.size.height);
     for (let i = 0; i < fieldHeight.length; i++) {
         const fieldWidth: Cell[] = new Array(settings.size.width);
@@ -25,7 +29,40 @@ function defaultField(settings: GameSettings): Field {
 export function generateField(settings: GameSettings): Field {
     // height - количество массивов [ [ 1 2 3 ] [4 5 6] ] - height: 2, width: 3
     // width - размер массива
-    // const positionsLeft = settings.size.width * settings.size.height;
+    // Заполнение нулевыми значениями
     const field = defaultField(settings);
+    // Заполнение минами
+    let mines = Math.min(settings.mines, settings.size.height * settings.size.width - 10);
+    while (mines !== 0) {
+        const randomHeight = generateRandomNumber(0, settings.size.height - 1);
+        const randomWidth = generateRandomNumber(0, settings.size.width - 1);
+        if (!field[randomHeight][randomWidth].isMine) {
+            field[randomHeight][randomWidth].isMine = true;
+            mines--;
+        }
+    }
+    // Заполнение значения мин вокруг
+    field.forEach((row, rowIndex) => {
+        row.forEach((cell, cellIndex) => {
+            let surroundingMines = 0;
+            const shouldCheckRight = cellIndex + 1 < settings.size.width;
+            const shouldCheckLeft = cellIndex > 0;
+            const shouldCheckUp = rowIndex > 0;
+            const shouldCheckDown = rowIndex + 1 < settings.size.height;
+            if (shouldCheckUp) {
+                if (shouldCheckLeft && field[rowIndex - 1][cellIndex - 1].isMine) surroundingMines++;
+                if (field[rowIndex - 1][cellIndex].isMine) surroundingMines++;
+                if (shouldCheckRight && field[rowIndex - 1][cellIndex + 1].isMine) surroundingMines++;
+            }
+            if (shouldCheckLeft && field[rowIndex][cellIndex - 1].isMine) surroundingMines++;
+            if (shouldCheckRight && field[rowIndex][cellIndex + 1].isMine) surroundingMines++;
+            if (shouldCheckDown) {
+                if (shouldCheckLeft && field[rowIndex + 1][cellIndex - 1].isMine) surroundingMines++;
+                if (field[rowIndex + 1][cellIndex].isMine) surroundingMines++;
+                if (shouldCheckRight && field[rowIndex + 1][cellIndex + 1].isMine) surroundingMines++;
+            }
+            cell.minesAround = surroundingMines as MinesAround;
+        });
+    });
     return field;
 }
--- src/store/field/types.ts
+++ src/store/field/types.ts
@@ -1,4 +1,4 @@
-type MinesAround = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+export type MinesAround = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 
 export type Cell = {
     isMine: boolean;
